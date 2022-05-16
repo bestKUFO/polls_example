91089cd9aa616160538e1e560a81976e09d33261
bestKUFO <best149@hs.ac.kr>
2022-05-16 15:07:56 +0900

    ok

--- polls/models.py
+++ polls/models.py
@@ -1,10 +1,14 @@
+import datetime
 from django.db import models
 
 
 # Create your models here.
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateField('date Published')
+    pub_date = models.DateTimeField('date Published')
+
+    def was_published_recently(self):
+        return self.pub_date >= datetime.timezone.now() - datetime.timedelta(days=1)
 
     def __str__(self):
         return self.question_text  # 어떤 내용으로 보여질지 리턴해줌
